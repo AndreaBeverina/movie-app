c9ee130ca093cd130fd79467ff0af076953ced24
Andrea Beverina <andrea.beverina@gmail.com>
2022-05-25 12:03:24 +0200

    Creazione funzione per stampare serie TV

--- modules/api-modules.js
+++ modules/api-modules.js
@@ -1,5 +1,7 @@
 const URL_movie_list = "https://www.omdbapi.com/?apikey=7a628d44&s=star%20wars";
 
+
+/* chiama la web api e restituisce elenco di film */
 export const listMovies = () => {
     fetch(URL_movie_list)
     .then(response => response.json())
@@ -8,3 +10,18 @@ export const listMovies = () => {
         console.log(movies_result);
     })
 }
+
+/* chiama la web api e restituisce SOLO le serie TV */
+
+const ricercaSerie = "&type=series";
+
+export const listSeries = () => {
+    let URL_serie_list = URL_movie_list + ricercaSerie;
+    fetch(URL_serie_list)
+    .then(response => response.json())
+    .then(results => {
+        const series_result = results.Search;
+        console.log(series_result);
+    })
+}
+
