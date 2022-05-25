66aa53aadc92ae5aa6fe58486b6e6e0bd30d8b27
Andrea Beverina <andrea.beverina@gmail.com>
2022-05-25 15:49:45 +0200

    Creazione viewItems per migliorare visualizz. dati

--- modules/api-modules.js
+++ modules/api-modules.js
@@ -6,6 +6,17 @@ export const apiList = (s,type) => {
     .then(response => response.json())
     .then(results => {
         const final_result = results.Search;
-        console.log(final_result);
+        //console.log(final_result);
+        viewItems(final_result);
+    })
+}
+
+const viewItems = (items) => {
+    items.map((item) => {
+        console.group(item.Title);
+        console.log(item.Year);
+        console.log(item.Type);
+        console.log(item.Poster);
+        console.groupEnd();
     })
 }
